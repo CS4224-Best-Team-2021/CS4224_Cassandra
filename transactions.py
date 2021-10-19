3116a0d9afc1f3b578f260987a9e1f0ada5661f3
lebencwb <46550747+lebencwb@users.noreply.github.com>
2021-10-20 02:07:04 +0800

    Add functions for Stock-Level Transaction and Popular-Item Transaction

--- transactions.py
+++ transactions.py
@@ -65,10 +65,61 @@ def order_status_transaction(c_w_id, c_d_id, c_id):
 
 # Read-heavy transactions
 def stock_level_transaction(w_id, d_id, threshold, num_last_orders):
-    pass
+    num_below_threshold = 0
+    item_ids = set()
+    N = District.filter(D_W_ID=w_id, D_ID=d_id).consistency(READ_CONSISTENCY_LEVEL).get().D_NEXT_O_ID
+    order_lines = OrderLine.filter(OL_W_ID=w_id, OL_D_ID=d_id, OL_O_ID__ge=(N-num_last_orders))
+    for order_line in order_lines:
+        item_ids.add(order_line.OL_I_ID)
+    stocks = Stock.filter(S_W_ID=w_id, S_I_ID__in=list(item_ids)).consistency(READ_CONSISTENCY_LEVEL)
+    for stock in stocks:
+        if stock.S_QUANTITY < threshold:
+            num_below_threshold += 1
+    print(f"Total number of items with stock quantity at W_ID {w_id} below the threshold: {num_below_threshold}")
 
 def popular_item_transaction(w_id, d_id, num_last_orders):
-    pass
+    N = District.filter(D_W_ID=w_id, D_ID=d_id).consistency(READ_CONSISTENCY_LEVEL).get().D_NEXT_O_ID
+    orders = Order.filter(O_W_ID=w_id, O_D_ID=d_id, O_ID__ge(N-num_last_orders)).consistency(READ_CONSISTENCY_LEVEL)
+    S = orders.values_list('O_ID')
+    items = {}
+    popular_items = {}
+    max_quantity = 0
+    for x in S:
+        popular_items[x] = set()
+        items[x] = OrderLine.filter(OL_W_ID=w_id, OL_D_ID=d_id, OL_O_ID=x)
+        for i in items[x]:
+            curr_quantity = i.OL_QUANTITY
+            if curr_quantity < max_quantity:
+                continue
+            if curr_quantity > max_quantity:
+                popular_items[x] = set()
+                max_quantity = curr_quantity
+            popular_items[x].add(i)
+    print(f"District identifier (W_ID, D_ID): {w_id}, {d_id}")
+    print(f"Number of last orders to be examined L = {num_last_orders}")
+    distinct_items = {}
+    for x in S:
+        order_x = orders.filter(O_ID=x).get()
+        print(Order number {x} at {order_x.O_ENTRY_D})
+        customer_x = Customer.filter(C_W_ID=w_id, C_D_ID=d_id, C_ID=order_x.O_C_ID).consistency(READ_CONSISTENCY_LEVEL).get()
+        print(f"Customer name: {customer_x.C_FIRST}, {customer_x.C_MIDDLE}, {customer_x.C_LAST}")
+        print("Popular items ordered <item name | quantity ordered>:")
+        for p in tuple(popular_items[x]):
+            item_id = p.OL_I_ID
+            item = Item.filter(I_ID=item_id).get()
+            item_name = item.I_NAME
+            print(f"{item_name} | {p.OL_QUANTITY}")
+            if item_id not in distinct_items:
+                distinct_items[item_id] = {}
+                distinct_items[item_id]['name'] = item_name
+                distinct_items[item_id]['count'] = 0
+            distinct_items[item_id]['count'] += 1
+    print("Percentage of examined orders containing each popular item")
+    print("<item name> | <% of orders in S containing the item>")
+    total_num_orders = orders.count()
+    for iid in distinct_items:
+        print(f"{distinct_items[iid]['name']} | {distinct_items[iid]['count'] / total_num_orders * 100}")
+
 
 def top_balance_transaction():
     # Processing steps
