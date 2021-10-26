b3704027b31777996f5a53808253c01f8804e79e
teikjun <teikjunhci@gmail.com>
2021-10-27 00:24:00 +0800

    compute throughput statistics

---
+++ compute_throughput_statistics.py
@@ -0,0 +1,16 @@
+import csv
+
+with open('results/clients.csv') as f:
+    file_reader = csv.reader(f, delimiter=',')
+    throughputs = []
+    for row in file_reader:
+        transaction_throughput = float(row[2])
+        throughputs.append(transaction_throughput)
+    min_throughput = min(throughputs)
+    max_throughput = max(throughputs)
+    avg_throughput = sum(throughputs) / len(throughputs)
+
+with open('results/throughput.csv', 'w') as throughput_file:
+    file_writer = csv.writer(throughput_file)
+
+    file_writer.writerow([min_throughput, max_throughput, avg_throughput])
