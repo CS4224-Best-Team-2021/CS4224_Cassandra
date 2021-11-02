f916d10dfe3ab3b1c263dfd7466d903c8e8b14b4
teikjun <teikjunhci@gmail.com>
2021-11-02 22:02:20 +0800

    update clean data file

--- clean_data.py
+++ clean_data.py
@@ -1,11 +1,13 @@
+# This file is for cleaning data prior to using load_db_data.cql
+
 # replace all occurrences of null with -1 in order.csv
-with open('order.csv') as f:
+with open('project_files_4/data_files/order.csv') as f:
     newText=f.read().replace('null', '-1')
-with open('modified-order.csv', "w") as f:
+with open('project_files_4/data_files/modified-order.csv', "w") as f:
     f.write(newText)
     
 # replace all occurrences of null with empty value in order-line.csv 
-with open('order-line.csv') as f:
-    newText=f.read().replace('null', '')
-with open('modified-order-line.csv', "w") as f:
+with open('project_files_4/data_files/order-line.csv') as f:
+    newText=f.read().replace(',null,', ',,')
+with open('project_files_4/data_files/modified-order-line.csv', "w") as f:
     f.write(newText)
