0f48483b313e1fdc8f73609ee89163a81fc33c95
teikjun <teikjunhci@gmail.com>
2021-11-02 22:04:37 +0800

    update clean_data.py file

--- clean_data.py
+++ clean_data.py
@@ -2,12 +2,12 @@
 
 # replace all occurrences of null with -1 in order.csv
 with open('project_files_4/data_files/order.csv') as f:
-    newText=f.read().replace('null', '-1')
+    new_order_text=f.read().replace('null', '-1')
 with open('project_files_4/data_files/modified-order.csv', "w") as f:
-    f.write(newText)
+    f.write(new_order_text)
     
 # replace all occurrences of null with empty value in order-line.csv 
 with open('project_files_4/data_files/order-line.csv') as f:
-    newText=f.read().replace(',null,', ',,')
+    new_order_line_text=f.read().replace(',null,', ',,')
 with open('project_files_4/data_files/modified-order-line.csv', "w") as f:
-    f.write(newText)
+    f.write(new_order_line_text)
